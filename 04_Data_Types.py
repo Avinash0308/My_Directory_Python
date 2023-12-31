5c9aacfd7720ab034a303532eec019ac3e520a61
Avinash Agrawal <avinash03082003@gmail.com>
2024-01-01 01:20:22 +0530

    Beautify

--- 04_Data_Types.py
+++ 04_Data_Types.py
@@ -5,50 +5,53 @@ print(a)
 a = 2.35324
 print(a)
 print("Hello everyone my name is avinash agrawal and i'm a student of aks univeristy satna pursuing my Bt.ech degree from cse branch and this is not very good college but any thing is very well for this is this for me now i am typing what ever comes in mind i m not tthinking about it very well so it is not good")
-print("Type of a is ",type(a))
+print("Type of a is ", type(a))
 a = 234
-print("Type of a is ",type(a))
-a = 234343,4
-print("type of a is ",type(a))
+print("Type of a is ", type(a))
+a = 234343, 4
+print("type of a is ", type(a))
 a = "3q4"
 b = "a2342"
-a+=b
+a += b
 print(a)
 afd = True
 afs = False
 ad = None
-print(afd,afs,ad)
-com= complex(3,6)
+print(afd, afs, ad)
+com = complex(3, 6)
 print(com)
-print("The data type of com is ",type(com))
-list1 = [3,523,534,3,532,523,5335,25,2,5352,22.52,335,253,5252]
+print("The data type of com is ", type(com))
+list1 = [3, 523, 534, 3, 532, 523, 5335, 25, 2, 5352, 22.52, 335, 253, 5252]
 print(list1)
-print("The data type of list1 is ",type(list1))
-list2 = [[234,234,343],234,2.52,35.3,[234,3],"34324324",'c',["343",23232]]
+print("The data type of list1 is ", type(list1))
+list2 = [[234, 234, 343], 234, 2.52, 35.3, [
+    234, 3], "34324324", 'c', ["343", 23232]]
 print(list2)
-print("The data type of list1 is ",type(list2))
-tuplee = (3,534,32,(343,53534,"adffdfd"),("fdoifiH",34,'O',"fjdiirj"),"ffdsf")
+print("The data type of list1 is ", type(list2))
+tuplee = (3, 534, 32, (343, 53534, "adffdfd"),
+          ("fdoifiH", 34, 'O', "fjdiirj"), "ffdsf")
 print(tuplee)
-print("The data type of tuplee is ",type(tuplee))
-dict = {"Me":"You","Mine":"Yours","You":"Me","Yours":"Mine","Rock":"Roll",3:"me","Me":3}
+print("The data type of tuplee is ", type(tuplee))
+dict = {"Me": "You", "Mine": "Yours", "You": "Me",
+        "Yours": "Mine", "Rock": "Roll", 3: "me", "Me": 3}
 print(dict)
 a = 134234342343234
 a = "alnd"
 print(a)
 print("Knock Knock Tera Baap Aaya")
-print(10*5,"Jtt")
+print(10*5, "Jtt")
 print("Lak 28 Kudi da 47 Weight Kudi da")
 me = False
 me = True
-com = complex(8,0)
+com = complex(8, 0)
 print(com)
 me = None
 me = set
-bool  = True
+bool = True
 if bool == False:
     print(bool)
-print("5538" +  "38", type(a))
-kuchbhi = [1,3,3,3,3,3,3,[1,3,3,5,2],4.5,"me"]
+print("5538" + "38", type(a))
+kuchbhi = [1, 3, 3, 3, 3, 3, 3, [1, 3, 3, 5, 2], 4.5, "me"]
 print(kuchbhi)
 tuple = []
 print(tuple)
