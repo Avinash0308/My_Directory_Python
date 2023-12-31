bc4786568bd32a41d215848b1d199d253aa2c7e6
Avinash Agrawal <avinash03082003@gmail.com>
2024-01-01 01:35:32 +0530

    Updated Typecasting.

--- 05_typecasting.py
+++ 05_typecasting.py
@@ -2,23 +2,32 @@ a = "1"
 b = "21"
 c = a+b
 print(c)
-c = a+b
+
 #Explicit Typecasting
 a = int(a)
 b = int(b)
 c = a+b
 print(c)
+
 print(int(a) + int(b))
+
 # Implicit typecasting
 c = 1.9
 d = 9 #d is implicitly typecasted to float
 print(c+d)
+
 c = 23434324567333432
 d = 2.5
 print(c+d)
+
 a = '3'
 b = '3'
 print((int)(a) + (int)(b))
+
+ha = "233"
+hb = "2332"
+print(int(ha) + int(hb))
+
 ha = "1harry112"
 av = "11avi0308"
 print(int(ha) + int(av))
