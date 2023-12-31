9e3a18ef3e08a5aa050bd3ce61b7272654567ec9
Avinash Agrawal <avinash03082003@gmail.com>
2024-01-01 01:40:13 +0530

    Updated Lecture 6

---
+++ 06_Input.py
@@ -0,0 +1,10 @@
+a = input()
+print(a)
+print("The input is: ",a)
+
+a = input("now this is something new , give me your name :")
+print("You have typed: ",a)
+
+a = input("Please input the first number: ")
+b = input("Now enter the second number: ")
+print("The sum of the given number is: ",int(a)+int(b))
--- input.py
+++
@@ -1,8 +0,0 @@
-a = input()
-print(a)
-print("The input is :",a)
-a = input("now this is something new , give me your name :")
-print("You have typed :",a)
-a = input("Please input hte first number :")
-b = input("Now enter the second number :")
-print("The sum of the given number is :",int(a)+int(b))
