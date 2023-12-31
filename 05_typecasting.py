92cfcbddfad723c8309fa0e6135453bc494174e6
Avinash Agrawal <avinash03082003@gmail.com>
2024-01-01 01:38:13 +0530

    Updated Chapter 5

--- 05_typecasting.py
+++ 05_typecasting.py
@@ -16,7 +16,7 @@ c = 1.9
 d = 9 #d is implicitly typecasted to float
 print(c+d)
 
-c = 23434324567333432
+c = 23434324
 d = 2.5
 print(c+d)
 
@@ -27,6 +27,12 @@ print((int)(a) + (int)(b))
 ha = "233"
 hb = "2332"
 print(int(ha) + int(hb))
+
+c = 23434324567333432
+d = 2.5
+e = c+d
+print(e)
+print("Data type of e is: ", type(e))
 
 ha = "1harry112"
 av = "11avi0308"
