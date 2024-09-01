51f07d1ed87f355e4791af1c8c46ed498630f584
Avinash Agrawal <avinash03082003@gmail.com>
2024-09-02 01:08:08 +0530

    Virtual EnvironmenT.

---
+++ 36_Virtual_Environment.py
@@ -0,0 +1,6 @@
+import pandas as pd
+print(pd.__version__)
+
+# pip freeze : to list all packages installed
+# pip freeze > file.txt: to create file with all requirements
+# pip install -r file.txt: to install all requirements.
